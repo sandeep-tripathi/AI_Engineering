d361451de05cb26f282e63977648e32f8933b005
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-01 13:10:30 +0100

    Update filter_duration.py

--- HuggingFace/audio/filter_duration.py
+++ HuggingFace/audio/filter_duration.py
@@ -1,4 +1,5 @@
 import librosa
+import numpy as np
 
 # Create a list of durations
 old_durations_list = []
@@ -12,3 +13,9 @@ dataset = dataset.add_column("duration", old_durations_list)
 
 # Filter the dataset for sound under 6 sec
 filtered_dataset = dataset.filter(lambda d: d < 6.0, input_columns=["duration"], keep_in_memory=True)
+
+# Save new durations
+new_durations_list = filtered_dataset["duration"]
+
+print("Old duration:", np.mean(old_durations_list))
+print("New duration:", np.mean(new_durations_list))
