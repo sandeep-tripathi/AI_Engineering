5538046f28db8c6f78485ff95629b1a8c4d3df9e
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-02 18:13:12 +0100

    Create classify_review.py

---
+++ openai/classify_review.py
@@ -0,0 +1,14 @@
+# Define a function to return the minimum distance and its index
+def find_closest(query_vector, embeddings):
+  distances = []
+  for index, embedding in enumerate(embeddings):
+    dist = distance.cosine(query_vector, embedding)
+    distances.append({"distance": dist, "index": index})
+  return min(distances, key=lambda x: x["distance"])
+
+for index, review in enumerate(reviews):
+  # Find the closest distance and its index using find_closest()
+  closest = find_closest(review_embeddings[index], class_embeddings)
+  # Subset sentiments using the index from closest
+  label = sentiments[closest['index']]['label']
+  print(f'"{review}" was classified as {label}')
